42c16580c01c0450811121855e492029d794f00d
David Zhou <david.zhou@thetradedesk.com>
2025-03-17 19:05:14 +1100

    start front navbar

--- main/frontend/src/App.tsx
+++ main/frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { User } from "./pages/User";
 import { Graph } from "./pages/Graph";
+import TopNavbar from "./components/TopNavbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <TopNavbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/user/:userId" element={<User />} />
